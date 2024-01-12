67baca18f85327609c7a9239da899c208ac4b499
Kevin <kevin.pallado97@gmail.com>
2024-01-13 02:21:56 +0800

    Revise prelimanary template to blend with vpn app

---
+++ resources/js/app.tsx
@@ -0,0 +1,16 @@
+import './bootstrap';
+import '../css/app.css';
+
+import { createRoot } from 'react-dom/client';
+import { createInertiaApp } from '@inertiajs/react';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+
+createInertiaApp({
+    title: (title) => `123myvpn - ${title}`,
+    resolve: (name) => resolvePageComponent(`./pages/${name}.tsx`, import.meta.glob('./pages/**/*.tsx')),
+    setup({ el, App, props }) {
+        const root = createRoot(el);
+
+        root.render(<App {...props} />);
+    },
+});
---
+++ resources/js/pages/home.tsx
@@ -0,0 +1,27 @@
+import CallToAction from '@/layouts/public/calltoaction'
+import Faqs from '@/layouts/public/faq'
+import Footer from '@/layouts/public/footer'
+import Header from '@/layouts/public/header'
+import Hero from '@/layouts/public/hero'
+import Pricing from '@/layouts/public/pricing'
+import PrimaryFeatures from '@/layouts/public/primaryfeatures'
+import SecondaryFeatures from '@/layouts/public/secondaryfeatures'
+import Testimonials from '@/layouts/public/testimonials'
+
+export default function Home() {
+    return (
+        <>
+            <Header headerTitle={'Home'} />
+            <main>
+                <Hero />
+                <PrimaryFeatures />
+                <SecondaryFeatures />
+                <CallToAction />
+                <Testimonials />
+                <Pricing />
+                <Faqs />
+            </main>
+            <Footer />
+        </>
+    )
+}
