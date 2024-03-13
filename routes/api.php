b8bbef1ea441867a26da23a3a7668f7e946e961b
Kevin <kevin.pallado97@gmail.com>
2024-03-14 04:54:59 +0800

    Added sub route for vpn to test in Iran

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,11 @@ use App\Http\Controllers\SubscribersController;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::domain('kor.korkhondi.com')->group(function () {
+    Route::get('test', function() {
+        return response()->json(['message' => 'Testing environment']);
+    });
+});
 Route::domain('api.123myvpn.com')->group(function () {
     Route::get('test', function() {
         return response()->json(['message' => 'Testing environment']);
