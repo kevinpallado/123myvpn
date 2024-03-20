ea42e5adb2227394dc57ed7d1bc33ffc407715b8
Kevin <kevin.pallado97@gmail.com>
2024-03-20 11:42:56 +0800

    Temporarily remove API routes

--- routes/api.php
+++ routes/api.php
@@ -18,27 +18,27 @@ use App\Http\Controllers\SubscribersController;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-Route::group(['prefix' => 'v1/subscriber', 'middleware' => 'throttle:subscriber'], function() {
-    // authentication
-    Route::group(['prefix' => 'auth'], function() {
-        Route::post('login', [LoginController::class, 'loginAPI']);
-        Route::post('register', [LoginController::class, 'subscriberRegisterAPI']);
-        Route::post('forgot-password', [LoginController::class, 'forgotPasswordAPI']);
-        Route::post('forgot-password-submit', [LoginController::class, 'forgotPasswordSubmitAPI']);
-    });
+// Route::group(['prefix' => 'v1/subscriber', 'middleware' => 'throttle:subscriber'], function() {
+//     // authentication
+//     Route::group(['prefix' => 'auth'], function() {
+//         Route::post('login', [LoginController::class, 'loginAPI']);
+//         Route::post('register', [LoginController::class, 'subscriberRegisterAPI']);
+//         Route::post('forgot-password', [LoginController::class, 'forgotPasswordAPI']);
+//         Route::post('forgot-password-submit', [LoginController::class, 'forgotPasswordSubmitAPI']);
+//     });
 
-    Route::group(['middleware' => 'auth:sanctum'], function () {
-        Route::post('change-password', [SubscribersController::class, 'subscriberChangePassword']);
-        Route::post('logout', [LoginController::class, 'logoutAPI']);
-        // subscriber profile
-        Route::group(['prefix' => 'profile'], function () {
-            Route::post('update', [SubscribersController::class, 'updateProfile']);
-        });
-        // subscriber data
-        Route::get('subscription-history', [SubscribersController::class, 'subscriberHistory']);
-        Route::get('pricing-plan', [SubscribersController::class, 'subscriberPriceListing']);
-        Route::get('subscription-status', [SubscribersController::class, 'subscriberDetails']);
-        Route::post('subscription-status', [SubscribersController::class, 'submitSubscription']);
-        Route::apiResource('vpn-servers', VPNServerController::class)->only('index');
-    });
-});
+//     Route::group(['middleware' => 'auth:sanctum'], function () {
+//         Route::post('change-password', [SubscribersController::class, 'subscriberChangePassword']);
+//         Route::post('logout', [LoginController::class, 'logoutAPI']);
+//         // subscriber profile
+//         Route::group(['prefix' => 'profile'], function () {
+//             Route::post('update', [SubscribersController::class, 'updateProfile']);
+//         });
+//         // subscriber data
+//         Route::get('subscription-history', [SubscribersController::class, 'subscriberHistory']);
+//         Route::get('pricing-plan', [SubscribersController::class, 'subscriberPriceListing']);
+//         Route::get('subscription-status', [SubscribersController::class, 'subscriberDetails']);
+//         Route::post('subscription-status', [SubscribersController::class, 'submitSubscription']);
+//         Route::apiResource('vpn-servers', VPNServerController::class)->only('index');
+//     });
+// });
